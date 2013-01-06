4858ae628fa6512c6511fc4adb51e317bbe2c7e5
jhberges <jhberges@gmail.com>
2013-01-06 21:40:53 +0100

    All in a sundays "work" -- but now LOTR 3 is on...

--- src/main/java/com/onsmsc/db/shovler/ShovlerBean.java
+++ src/main/java/com/onsmsc/db/shovler/ShovlerBean.java
@@ -1,5 +1,47 @@
 package com.onsmsc.db.shovler;
 
-public class ShovlerBean {
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
 
+public class ShovlerBean {
+	private static final long DEFAULT_BATCH_SIZE = 200;
+	private ConnectionFactory connectionFactory;
+	private Connection connection;
+	private String jmsPassword;
+	private String jmsUsername;
+	private Session session;
+	private Destination destination;
+	private MessageConsumer consumer;
+	private final long maxBatchSize = DEFAULT_BATCH_SIZE;
+	public void process() {
+		while(running) {
+			try {
+				ensureInitialized();
+				processInLoop();
+			} catch (JMSException jmsException) {
+				pauseOnException(jmsException);
+			}
+		}
+	}
+
+	private void processInLoop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void pauseOnException(JMSException jmsException) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ensureInitialized() throws JMSException {
+		connection = connectionFactory.createConnection(jmsUsername, jmsPassword);
+		session = connection.createSession(true, Session.CLIENT_ACKNOWLEDGE);
+		consumer = session.createConsumer(destination);
+		connection.start();
+	}
 }
